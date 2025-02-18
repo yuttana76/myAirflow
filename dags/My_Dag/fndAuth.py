01639a6d425382e2ebac0d5cc57383efc29ee9e5
MPAM DEV03 <mpamdev03@MPAMs-MacBook-Air.local>
2025-02-18 13:05:07 +0700

    Download fund profile

--- dags/My_Dag/fndAuth.py
+++ dags/My_Dag/fndAuth.py
@@ -1,34 +1,58 @@
+import sys
 import requests
 import zipfile
+import os
+from datetime import datetime
+from datetime import timedelta
 
-# Example usage:
-api_url = "" 
-data = {
-  "username": "",
-  "password": ""
-}
+from airflow import DAG
+# We need to import the operators used in our tasks
+from airflow.operators.python_operator import PythonOperator
+# We then import the days_ago function
+from airflow.utils.dates import days_ago
 
-def getToken(url, data):
+
+
+def T_GetToken():
+  print("** getToken.")
   
   try:
-    response = requests.post(url, json=data)
+
+    api_url = os.environ["FC_API_URL"]+"/auth"
+    data = {
+      "username": os.environ["FC_API_USER"],
+      "password": os.environ["FC_API_PASSPOWRD"]
+    }
+
+    response = requests.post(api_url, json=data)
 
     response.raise_for_status()  # Raise an exception for bad status codes
     response_data = response.json()
-    return response_data.get('access_token') 
+    token = response_data.get('access_token')  
+    return token
   
     # return response
   except requests.exceptions.RequestException as e:
     print(f"Error making POST request: {e}")
-    return None
+    sys.exit(1)
+    # return None
 
 
-def get_downloadRequest(token):
+def T_DownloadFile(token,fileType):
   
-  print("TOKEN>> %s",token)
+  print("** downloadFile.")
+
+  download_file_path = f"{rawDataPpath}/{fileType}.zip"
+
+  businessDate = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
+  businessDate = datetime.now().strftime("%Y%m%d")
+
+  print(f"-businessDate: {businessDate}")
+  print(f"-fileType: {fileType}")
+  print(f"-token: {token}")
 
   try:
-    url = "https://stage.fundconnext.com/api/files/20250207/FundProfile.zip" 
+    url = os.environ["FC_API_URL"]+f"/files/{businessDate}/{fileType}.zip" 
     headers = {
     "X-Auth-Token": token,
     "Content-Type": "application/json"
@@ -41,44 +65,64 @@ def get_downloadRequest(token):
       for chunk in response.iter_content(chunk_size=8192): 
         f.write(chunk)
 
-    return response
+    # Save file
+    headers = response.headers
+    if headers:
+      content_type = headers.get('Content-Type')
+      content_disposition = headers.get('Content-Disposition')
+
+      if content_type == 'application/zip' and content_disposition:
+        filename = content_disposition.split(';')[1].strip().split('=')[1].strip('"') 
+        print(f"Content-Disposition: {content_disposition}")
+        print(f"Filename: {filename}") 
+
+    # Exptrtact zip file
+    extract_path = "./data/raw_data"
+    try:
+      with zipfile.ZipFile(download_file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_path)
+      return True
+    except Exception as e:
+      print(f"Error extracting ZIP file: {e}")
+      sys.exit(1)
+
+
+    return download_file_path
   except requests.exceptions.RequestException as e:
-    print(f"Error making GET request: {e}")
-    return None
+    print(f"Error download file and save: {e}")
+    sys.exit(1)
 
-def extract_zip(zip_file_path, extract_path):
 
-  try:
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-      zip_ref.extractall(extract_path)
-    return True
-  except Exception as e:
-    print(f"Error extracting ZIP file: {e}")
-    return False
-  
-download_file_path = "./raw_data/downloaded_file.zip" 
+# Example usage:
+fileType = "FundProfile"  
+rawDataPpath = "./data/raw_data"
+token=""
 
-token = getToken(api_url, data)
-response = get_downloadRequest(token)
+# Test 
+# token=T_GetToken(api_url, data)
+# T_DownloadFile(token,fileType)
 
-headers = response.headers
-if headers:
-  content_type = headers.get('Content-Type')
-  content_disposition = headers.get('Content-Disposition')
 
-  if content_type == 'application/zip':
-    print(f"Content-Type: {content_type}")
+# initializing the default arguments that we'll pass to our DAG
+with DAG(
+        'fundConnext_FundProfile_dag',
+        start_date=datetime(2025, 1, 1),
+        schedule_interval="* 8 * * 1-5", 
+        catchup=False
+) as dag:
 
-    if content_disposition:
-      filename = content_disposition.split(';')[1].strip().split('=')[1].strip('"') 
-      print(f"Content-Disposition: {content_disposition}")
-      print(f"Filename: {filename}") 
+    task1 = PythonOperator(
+        task_id='task1',
+        python_callable=T_GetToken,
+        dag=dag,
+        do_xcom_push=True 
+    )
 
-# Example Usage:
-zip_file_path = download_file_path
-extract_path = "./raw_data"
+    task2 = PythonOperator(
+        task_id='task2',
+        python_callable=T_DownloadFile,
+        op_kwargs={'token': '{{ ti.xcom_pull(task_ids="task1") }}', 'fileType': fileType},
+        dag=dag
+    )
 
-if extract_zip(zip_file_path, extract_path):
-  print(f"ZIP file extracted successfully to: {extract_path}")
-else:
-  print("Failed to extract ZIP file.")
+    task1 >> task2 
