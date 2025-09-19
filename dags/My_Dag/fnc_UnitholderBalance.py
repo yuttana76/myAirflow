3a0d630fa9e9efc20ddc13a55a503f2c4bc20ebc
MPAM DEV03 <mpamdev03@MPAMs-MacBook-Air.local>
2025-09-19 15:53:58 +0700

    fix xcom unitholser

--- dags/My_Dag/fnc_UnitholderBalance.py
+++ dags/My_Dag/fnc_UnitholderBalance.py
@@ -187,7 +187,7 @@ with DAG(
     task3 = PythonOperator(
         task_id='pg_upsert_unitholderBalance',
         python_callable=T_postgres_upsert_dataframe,
-        op_kwargs={'fileName': '{{ ti.xcom_pull(task_ids="downloadFiles_evening") }}'},
+        op_kwargs={'fileName': '{{ ti.xcom_pull(task_ids="dwn_fnc_unitholderBalance") }}'},
         # op_kwargs={'fileName': '20250917_MPS_UNITHOLDERBALANCE.txt'},
         on_failure_callback=notify_teams,
     )
